a18c11edefdc8db342708f9c76c3219609575a4a
yangwj <245066263@qq.com>
2021-10-10 00:48:46 +0800

    1、menu菜单

--- app/src/main/java/com/ywj/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/ywj/kotlindemo/MainActivity.kt
@@ -1,14 +1,36 @@
 package com.ywj.kotlindemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        tvTest.setOnClickListener {
+            Toast.makeText(this, "点击了", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add_item -> Toast.makeText(this, "add", Toast.LENGTH_SHORT).show()
+            R.id.remove_item -> Toast.makeText(this, "remove", Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
